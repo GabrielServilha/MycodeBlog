8baa047b95dc8209f3519e131138943d748ac0c3
GabrielServilha <gabriel.teodoro2000@hotmail.com>
2022-05-22 18:17:16 -0300

    frist commit

---
+++ src/main/java/com/spring/codeblog/service/serviceimpl/CodeblogServiceImpl.java
@@ -0,0 +1,34 @@
+package com.spring.codeblog.service.serviceimpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import com.spring.codeblog.service.CodeblogService;
+
+
+@Service
+public class CodeblogServiceImpl implements CodeblogService {
+
+	@Autowired
+	CodeblogRepository codeblogRepository;
+
+	@Override
+	public List<Post> findAll() {
+		return codeblogRepository.findAll();
+	}
+
+	@Override
+	public Post FindById(long id) {
+		return codeblogRepository.findById(id).get();
+	}
+
+	@Override
+	public Post savePost(Post post) {
+		return codeblogRepository.save(post);
+	}
+
+}
